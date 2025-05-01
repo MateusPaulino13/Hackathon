ca2329346a3d251bd73ee83fe0416e980c91e0be
Fernando Alvaro Facioli <132278054+FerFacioli@users.noreply.github.com>
2025-05-01 20:14:44 -0300

    Correção fint.php e navbar.html

--- pages/fint.php
+++ pages/fint.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/styles/fint.css">
+    <link rel="shortcut icon" href="../assets/images/8.png" type="image/x-icon" />
     <title>Remédios Naturais</title>
 </head>
 
