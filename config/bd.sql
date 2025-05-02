aa4a52951df4464dc0087dae184eafb64120019c
Mateus Paulino <mateusgpaulino@gmail.com>
2025-05-01 23:30:05 -0300

    adicionando auto_increment aos ids
    adicionei auto_increment para as tabelas aonde tem ids para identificar colunas, assim n será necessário que adicione manualmente

--- config/bd.sql
+++ config/bd.sql
@@ -2,18 +2,18 @@ CREATE DATABASE if not exists hackaton;
 
 USE hackaton;
 
-CREATE TABLE users(
-    id INT PRIMARY KEY,
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name CHAR(100),
     email CHAR(100),
     password_hash CHAR(50),
     created_at DATE,
-    height int,
-    weight float
+    height INT,
+    weight FLOAT
 );
 
-create table daily_entries(
-    id INT PRIMARY KEY,
+CREATE TABLE daily_entries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id),
     entry_date DATE,
@@ -21,63 +21,63 @@ create table daily_entries(
 );
 
 CREATE TABLE nutrition_entries (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     daily_entry_id INT,
     fruits ENUM('none', 'once', 'more_than_once'),
     vegetables ENUM('none', 'once', 'more_than_once'),
     ultra_processed ENUM('none', 'once', 'more_than_once'),
-        FOREIGN KEY (daily_entry_id) REFERENCES daily_entries(id)
+    FOREIGN KEY (daily_entry_id) REFERENCES daily_entries(id)
 );
 
-create table exercise_entries (
-    id int PRIMARY KEY,
-    daily_entry_id int,
-        FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
-    did_exercise boolean,
-    duration_minutes int
+CREATE TABLE exercise_entries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    daily_entry_id INT,
+    FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
+    did_exercise BOOLEAN,
+    duration_minutes INT
 );
 
-create table water_entries (
-    id int PRIMARY KEY,
-    daily_entry_id int,
-        FOREIGN KEY (daily_entry_id) REFERENCES daily_entries(id),
-    cups_drank int
+CREATE TABLE water_entries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    daily_entry_id INT,
+    FOREIGN KEY (daily_entry_id) REFERENCES daily_entries(id),
+    cups_drank INT
 );
 
-create table sun_entries (
-    id int PRIMARY KEY,
-    daily_entry_id int,
-        FOREIGN KEY (daily_entry_id) REFERENCES daily_entries(id),
-    exposed boolean,
-    duration_minutes int
+CREATE TABLE sun_entries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    daily_entry_id INT,
+    FOREIGN KEY (daily_entry_id) REFERENCES daily_entries(id),
+    exposed BOOLEAN,
+    duration_minutes INT
 );
 
-create table temperance_entries (
-    id int PRIMARY KEY,
-    daily_entry_id int,
-        FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
-    drank_alcohol boolean,
+CREATE TABLE temperance_entries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    daily_entry_id INT,
+    FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
+    drank_alcohol BOOLEAN,
     used_tobacco BOOLEAN
 );
 
-create table air_entries (
-    id int PRIMARY KEY,
-    daily_entry_id int,
-        FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
-    outdoor_activity boolean
+CREATE TABLE air_entries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    daily_entry_id INT,
+    FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
+    outdoor_activity BOOLEAN
 );
 
-create table rest_entries (
-    id int PRIMARY KEY,
-    daily_entry_id int,
-        FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
-    slept_7_8h boolean,
-    slept_before_22h boolean
+CREATE TABLE rest_entries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    daily_entry_id INT,
+    FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
+    slept_7_8h BOOLEAN,
+    slept_before_22h BOOLEAN
 );
 
-create table trust_entries (
-    id int PRIMARY KEY,
-    daily_entry_id int,
-        FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
-    spiritual_activity boolean
+CREATE TABLE trust_entries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    daily_entry_id INT,
+    FOREIGN KEY(daily_entry_id) REFERENCES daily_entries(id),
+    spiritual_activity BOOLEAN
 );
